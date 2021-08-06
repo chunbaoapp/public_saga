2b8ca89ac5273f0b9ab2091fa77a6aebca55b223
AMS\Yang_C <chunbao.yang@schiphol.nl>
2021-08-06 21:04:59 +0200

    improve

--- src/main/java/chunbao/nl/event/sagas/SagasApplication.java
+++ src/main/java/chunbao/nl/event/sagas/SagasApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EntityScan("chunbao.nl.event.module") // scan h2 entity
-@ComponentScan("chunbao.nl.event.dao") // scan repo beans
+@EntityScan(
+    basePackages = {
+      "chunbao.nl.event.user",
+      "chunbao.nl.event.saga",
+      "chunbao.nl.event.event"
+    }) // scan h2 entity
+@ComponentScan(basePackages = {"chunbao.nl.event.dao"}) // external beans
 public class SagasApplication {
 
   public static void main(String[] args) {
